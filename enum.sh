d184d88c6e40cabf01232dd115a99ed6055373af
anon <magesalexander123@gmail.com>
2020-11-13 15:46:53 -0600

    first

---
+++ enum.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo "eneumerate dis bih" | cowsay | lolcat
+
+echo "note: uniscan functionality requires password entry unless script is ran as root"
+
+printf "#Enter name of workspace folder /home/anon/Workspace/* \n>"
+
+	read workspacefolder
+
+	cd /home/anon/Workspace/$workspacefolder
+
+
+printf "#Enter RHOST w/o additional formatting \n>"
+
+	read preRHOST
+
+	RHOST="http://${preRHOST}/"
+
+printf "#Enter wordlist size for gobuster small/medium/large/dirb_default\n>"
+	        read wordlistsize
+                if [ $wordlistsize == "small" ]
+                        then
+                        wordlist="/usr/share/wordlists/dirbuster/directory-list-2.3-small.txt"
+                elif [ $wordlistsize == "medium" ]
+                        then
+                        wordlist="/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt"
+                elif [ $wordlistsize == "large" ]
+                        then
+                        wordlist="/usr/share/wordlists/dirb/big.txt"
+                elif [ $wordlistsize == "dirb_default" ]
+			then
+			wordlist="/usr/share/wordlists/dirb/common.txt"
+fi
+	printf "#Do you want to use brute force extensions too? yes/no(default) (you can master quantum physics while you wait)\n>"
+	read extensions
+
+if [ extensions == "yes" ]
+then
+extention="-x /usr/share/wordlists/dirb/extentions-common.txt"
+fi
+
+#note to concatenate extensions onto command
+tab=" --tab"
+options=()
+
+cmds[1]="nikto -h ${RHOST}"
+
+cmds[2]="sudo uniscan -u ${RHOST} -qweds"
+
+cmds[3]="gobuster dir -u ${RHOST} -w ${wordlistsize} ${extension}"
+
+for i in 1 2; do
+options+=($tab -e "bash -c '${cmds[i]} ; bash'" )
+done
+
+gnome-terminal "${options[@]}"
+exit 0
