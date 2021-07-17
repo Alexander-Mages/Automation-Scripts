eaaae9d5932e19afd7483528c20f55c36b68eaee
Alex Mages <73716189+Alexander-Mages@users.noreply.github.com>
2021-07-17 11:14:58 -0500

    typo fix

--- anonreq.py
+++ anonreq.py
@@ -4,8 +4,7 @@ from burp import IHttpService
 from burp import IHttpRequestResponse
 from burp import IHttpListener
 
-HOST_FROM = "zooms.us"
-HOST_TO = "zoom.us"
+HOST = "host.com"
 NUMREP = 2
 
 class BurpExtender(IBurpExtender, IHttpListener, IHttpService, IHttpRequestResponse):
@@ -20,8 +19,8 @@ class BurpExtender(IBurpExtender, IHttpListener, IHttpService, IHttpRequestRespo
 				return
 
 			httpService = messageInfo.getHttpService()
-			if (HOST_FROM == httpService.getHost()):
-				messageInfo.setHttpService(self._helpers.buildHttpService(HOST_TO,httpService.getPort(), httpService.getProtocol()))
+			if (HOST == httpService.getHost()):
+				messageInfo.setHttpService(self._helpers.buildHttpService(HOST,httpService.getPort(), httpService.getProtocol()))
 			request = IHttpRequestResponse.getRequest()
 			for i in range(NUMREP):
 				IHttpRequestResponse.MakeHttpRequest(httpService, request)
